9760f1c2159911cb82decc00a69ae0e3e61397c2
Shengela <168460082+Shengelaa@users.noreply.github.com>
2025-05-19 02:44:07 +0400

    rr

--- api/data.js
+++ api/data.js
@@ -6,6 +6,9 @@ const client = new MongoClient(uri);
 
 export default async function handler(req, res) {
   try {
+    if (!process.env.MONGODB_URI) {
+      throw new Error("MONGODB_URI is not defined in environment variables");
+    }
     if (!client.isConnected?.()) await client.connect();
     const db = client.db("mydb");
     const collection = db.collection("entries");
